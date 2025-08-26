c70a4f2face803abfed3390f9a4c2b6603bf2ec9
KhoaDN <dnk270204@gmail.com>
2025-08-26 14:02:46 +0700

    setup state machine

---
+++ HBAcademy/Assets/_Project/Scripts/Systems/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using Vox.Ultilities.StateMachine;
+
+public class PlayerController : StateController<PlayerController.PlayerState>
+{
+    public enum PlayerState
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump,
+        Attack
+    }
+
+    void Awake()
+    {
+        _states[PlayerState.Idle] = new IdleState(PlayerState.Idle);
+
+        _currentState = _states[PlayerState.Idle];
+    }
+
+    private void Update()
+    {
+        if (_currentState == _states[PlayerState.Idle]) Debug.Log("Player is Idle");
+    }
+}
---
+++ HBAcademy/Assets/_Project/Scripts/Ultilities/DesignPatterns/StateMachine/BaseState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vox.Ultilities.StateMachine
+{
+    /// <summary>
+    /// BaseState - Developed by Duong Nhat Khoa on 2025/08/26. <br/>
+    /// A base class for defining states in a state machine.
+    /// </summary>
+    /// <typeparam name="EState"> Object Enum. </typeparam>
+    public abstract class BaseState<EState> where EState : Enum
+    {
+        #region --- Constructors ---
+
+        public BaseState(EState key)
+        {
+            StateKey = key;
+        }
+
+        #endregion
+
+        #region --- Methods ---
+
+        public abstract void EnterState();
+        public abstract void UpdateState();
+        public abstract void ExitState();
+        public abstract EState GetNextState();
+        public abstract void OnTriggerEnter(Collider other);
+        public abstract void OnTriggerExit(Collider other);
+        public abstract void OnTriggerStay(Collider other);
+
+        #endregion
+
+        #region --- Properties ---
+
+        public EState StateKey { get; private set; }
+
+        #endregion
+    }
+}
